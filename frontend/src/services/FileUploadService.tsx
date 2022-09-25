8fd14dcb3a5ed6bc680b6ae7b37c92275dc9fee1
Francisco Mahay <javier.mahay@gmail.com>
2022-09-25 16:34:01 -0500

    reverted url

--- frontend/src/services/FileUploadService.tsx
+++ frontend/src/services/FileUploadService.tsx
@@ -2,13 +2,10 @@ const upload = async (file: File): Promise<Response | void> => {
   let formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch(
-    "https://grainchain-challenge.herokuapp.com/api/home/upload-file",
-    {
-      method: "POST",
-      body: formData,
-    }
-  );
+  const response = await fetch("http://localhost:5001/api/home/upload-file", {
+    method: "POST",
+    body: formData,
+  });
 
   return response.json();
 };
