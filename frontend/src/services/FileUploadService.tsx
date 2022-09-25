ac874eb2b2652f4bbc4981386b8b1efe17c754a1
Francisco Mahay <javier.mahay@gmail.com>
2022-09-25 16:27:36 -0500

    fixed url service

--- frontend/src/services/FileUploadService.tsx
+++ frontend/src/services/FileUploadService.tsx
@@ -2,10 +2,13 @@ const upload = async (file: File): Promise<Response | void> => {
   let formData = new FormData();
   formData.append("file", file);
 
-  const response = await fetch("http://localhost:5001/api/home/upload-file", {
-    method: "POST",
-    body: formData,
-  });
+  const response = await fetch(
+    "https://grainchain-challenge.herokuapp.com/api/home/upload-file",
+    {
+      method: "POST",
+      body: formData,
+    }
+  );
 
   return response.json();
 };
